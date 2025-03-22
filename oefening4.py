d971ca755ed5cf125431e0d9b1f37f59f6db0086
Zita Vekemans <zita.vekemans@leerling.athenea.be>
2025-03-22 15:21:37 +0100

    oefening 4

--- oefening4.py
+++ oefening4.py
@@ -1,1 +1,16 @@
-def countTargetPairs():
+def countTargetPairs(nums,target):
+    n = len(nums)
+    count = 0
+
+    for i in range(n):
+        for j in range(i + 1 ,n):
+            if nums[i] + nums[j] < target:
+                count += 1
+
+    return count
+
+
+nums = [-1,1,2,3,1]
+target = 2
+antwoord = countTargetPairs(nums,target)
+print(antwoord)
